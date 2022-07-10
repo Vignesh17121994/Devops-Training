4567925be1e3b91d66ea9dbbe09f3b30ebf12b34
Vignesh17121994 <vigneshwaran1712@gmail.com>
2022-07-10 19:23:20 +0530

    update

--- staragile_awsdevops_20220625-main/DAY-4_20220703/ec2-ubuntu-Jenkins.sh
+++ staragile_awsdevops_20220625-main/DAY-4_20220703/ec2-ubuntu-Jenkins.sh
@@ -5,8 +5,8 @@ aws ec2 run-instances \
     --image-id "ami-068257025f72f470d" \
     --instance-type t2.micro \
     --count 1 \
-    --subnet-id "subnet-5183ac39" \
-    --key-name "awskeys-mum" \
-    --security-group-ids "sg-0aa1782637f5f7dd9" \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins},{Key=Type,Value=CI},{Key=ProjectName,Value=CloudBinary},{Key=Environment,Value=Dev}]' \
+    --subnet-id "subnet-0b48668dd707a05e1" \
+    --key-name "aws-linux-key" \
+    --security-group-ids "sg-00d44bcb68a6539b5" \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins},{Key=Type,Value=CI},{Key=ProjectName,Value=Techhevicky},{Key=Environment,Value=Dev}]' \
     --user-data file://jenkins.txt 
